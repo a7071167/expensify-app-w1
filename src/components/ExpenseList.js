456ab276ddfcca383ee9341b4d99c530ad5d86d1
Kufel Alexander <kufelalexander@gmail.com>
2020-04-08 11:04:26 +0300

    Add expense summary to dashboard

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -2,10 +2,11 @@ import React from 'react'
 import { connect } from 'react-redux' 
 import ExpenseListItem from './ExpenseListItem'
 import selectExpenses from '../selectors/expenses'
+//import ExpenseListTotal from './ExpenseListTotal'
 
 const ExpenseList = (props) => (
     <div>
-        <h1>Expense List</h1>
+        {props.expenses.length === 0 ? <p>No Expenses</p> : <h1>Expense List</h1>}
         {props.expenses.map((expense) => (<ExpenseListItem key={expense.id} {...expense}/>))}
     </div>
 )
